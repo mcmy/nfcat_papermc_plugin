6e1ce3b37c154c3687ffd47292095319b359ab63
mcmy <775720345@qq.com>
2022-09-27 19:31:04 +0800

    项目

--- src/main/java/com/nfcat/papermc/commands/shop/GuiShop.java
+++ src/main/java/com/nfcat/papermc/commands/shop/GuiShop.java
@@ -1,5 +1,6 @@
 package com.nfcat.papermc.commands.shop;
 
+import com.destroystokyo.paper.event.player.PlayerPickupExperienceEvent;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -7,13 +8,20 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPickupArrowEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import javax.sound.sampled.LineListener;
+
 public class GuiShop implements CommandExecutor {
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         Inventory gui = Bukkit.createInventory(null, 6 * 9, Component.text("shop"));
